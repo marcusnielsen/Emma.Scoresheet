005a50806d5044d5aae2543eef7bef875d52836c
Marcus Nielsen <marcus.nielsen@r2m.se>
2013-12-06 12:27:28 +0100

    adding missing files.

---
+++ Emma.Scoresheet/app/scoresheet/mnScoresheet.js
@@ -0,0 +1,16 @@
+﻿appModule
+.directive('mnScoresheet', function () {
+    return {
+        restrict: 'A',
+        scope: {
+            //'mnScoresheetModel' : '='
+        },
+        replace: true,
+        link: function (scope, element, attrs) {
+             //scope.
+        },
+        template:"<h1>Test</h1>"
+    }
+});
+
+
