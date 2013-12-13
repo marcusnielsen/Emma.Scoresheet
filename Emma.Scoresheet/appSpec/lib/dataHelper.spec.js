e41d8bc5c2e28c90e649f2a97c25162d2a29a926
Marcus Nielsen <marcus.nielsen@r2m.se>
2013-12-13 09:22:46 +0100

    Added first spec for dateHelper.js

---
+++ Emma.Scoresheet/appSpec/lib/dataHelper.spec.js
@@ -0,0 +1,6 @@
+﻿describe("dateHelper.js", function () {
+    it("should return the date in format yyyy-mm-dd", function () {
+        var date = new Date("2013-12-24T08:00:00");
+        expect(date.yyyymmdd()).toBe("2013-12-24");
+    });
+});
