3c60da3d23a551845afc2f4cf812e0255ffa909b
Marcus Nielsen <marcus.nielsen@r2m.se>
2013-12-17 15:08:19 +0100

    Added spec for mnInput

--- Emma.Scoresheet/uiSpec/spec/app.spec.js
+++ Emma.Scoresheet/uiSpec/spec/app.spec.js
@@ -1,9 +1,6 @@
 describe('index.html', function() {
   var ptor = protractor.getInstance();
-
-  beforeEach(function() {
-    ptor.get('#/');
-  });
+  ptor.get('#/');
 
   it('should have a page header with text "EMMA european mobile media association"', function () {
     var title = ptor.findElement(protractor.By.className('page-header'));
