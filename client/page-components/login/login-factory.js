220df869038150b5d94d3cb237c22fc13dd3d548
Marcus Nielsen <marcus.nielsen82@gmail.com>
2014-08-15 13:49:34 +0200

    Small refactor.

--- client/page-components/login/login-factory.js
+++ client/page-components/login/login-factory.js
@@ -3,26 +3,27 @@
 module.exports = ['$rootScope', '$http', 'mnUserFactory', function ($rootScope, $http, mnUserFactory) {
   var loginFactory = {};
 
+  var onLoggedIn = function (userData) {
+    loginFactory.clearLoginData();
+    loginFactory.isLoggedIn = true;
+    mnUserFactory.userData = userData;
+    $rootScope.$broadcast('login-logged-in');
+  };
+
   loginFactory.register = function () {
-    $http.post('api/users', loginFactory.loginData).then(function () {
-      //TODO: Something?
+    $http.post('api/users', loginFactory.loginData).then(function (res) {
+      onLoggedIn(res.data);
     });
   };
 
   loginFactory.login = function () {
     $http.post('api/login', loginFactory.loginData).then(function (res) {
-      mnUserFactory.userData = res.data;
-
-      //TODO: Cleanup & refactor isLoggedIn
-      loginFactory.isLoggedIn = true;
-
-      $rootScope.$broadcast('login-logged-in');
+      onLoggedIn(res.data);
     });
   };
 
   loginFactory.logout = function () {
     loginFactory.isLoggedIn = false;
-
     $rootScope.$broadcast('login-logged-out');
   };
 
--- client/page-components/user/user-factory.js
+++ client/page-components/user/user-factory.js
@@ -4,8 +4,6 @@ module.exports = [function () {
   var userFactory = {};
 
   userFactory.userData = {};
-  userFactory.userData.name = '';
-  userFactory.userData.email = '';
 
   return userFactory;
 }];
