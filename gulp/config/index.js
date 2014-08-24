25933d47d9b250fe3512f336da8e8fcc93f710c6
Marcus Nielsen <marcus.nielsen82@gmail.com>
2014-08-24 22:17:19 +0200

    'key' => key.

--- gulp/config/index.js
+++ gulp/config/index.js
@@ -1,36 +1,36 @@
 'use strict';
 
 var config = {
-  'clean': './dist',
-  'nodemon': {
-    'start': './server/server.js',
-    'source': './server',
-    'ext': 'html js'
+  clean: './dist',
+  nodemon: {
+    start: './server/server.js',
+    source: './server',
+    ext: 'html js'
   },
-  'browserify': {
-    'source': './client/app-module.js',
-    'dest': {
-      'filename': 'bundle.js',
-      'path': './dist/js'
+  browserify: {
+    source: './client/app-module.js',
+    dest: {
+      filename: 'bundle.js',
+      path: './dist/js'
     },
     sourceMapFileName: 'bundle.map.json'
   },
-  'html': {
-    'source': './client/**/*.html',
-    'dest': './dist/html'
+  html: {
+    source: './client/**/*.html',
+    dest: './dist/html'
   },
-  'less': {
-    'source': './client/**/*.less',
-    'dest': './dist/css',
-    'sourcemaps': './maps'
+  less: {
+    source: './client/**/*.less',
+    dest: './dist/css',
+    sourcemaps: './maps'
   },
-  'localization': {
-    'source': './client/localization/**/*',
-    'dest': './dist/localization'
+  localization: {
+    source: './client/localization/**/*',
+    dest: './dist/localization'
   },
-  'lint': {
+  lint: {
     //TODO: Enable linting for tests when tests are stable.
-    'source': ['!./node_modules/**/*', '!./dist/**/*', '!./server/test/**/*', './**/*.js']
+    source: ['!./node_modules/**/*', '!./dist/**/*', '!./server/test/**/*', './**/*.js']
   }
 };
 
