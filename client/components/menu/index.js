d7906a3b4f3720200fc1752f99580fcd823bad9a
Marcus Nielsen <marcus.nielsen82@gmail.com>
2014-08-30 20:55:58 +0200

    Fixed right side login button.

--- client/components/menu/index.js
+++ client/components/menu/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = angular.module('mnMenu', [])
+module.exports = angular.module('mnMenu', [require('../../page-components/user').name])
   .factory('mnMenuFactory', require('./menu-factory'))
   .controller('mnMenuController', require('./menu-controller'))
   .directive('mnMenu', require('./menu-directive'));
--- client/components/menu/menu-factory.js
+++ client/components/menu/menu-factory.js
@@ -1,14 +1,12 @@
 'use strict';
 
-module.exports = ['$state', function ($state) {
+module.exports = ['$rootScope', '$state', 'mnUserFactory', function ($rootScope, $state, mnUserFactory) {
   var factory = {
     brandMenuItem: {title: 'EMMA', sref: 'home'},
     //TODO: These links should be displayed according to the users group policy.
     navigationItems: [
       {title: '$HOME', sref: 'home'},
-      {title: '$LOGIN', sref: 'login'},
       {title: '$SETTINGS', sref: 'setting'},
-      {title: '$USER', sref: 'user'},
       {title: '$VEHICLES', sref: 'vehicle'},
       {title: '$COMPETITIONS', sref: 'competition'},
       {title: '$SCORESHEET_TEMPLATES', sref: 'scoresheet-template'}
@@ -19,9 +17,27 @@ module.exports = ['$state', function ($state) {
     return $state.includes(sref);
   };
 
+  factory.loginSref = 'login';
+  factory.userSref = 'user';
+
+  // TODO: Refactor dist/html/components/menu/ here and in the directive.
+  factory.userLoginTemplate = 'dist/html/components/menu/login-or-register.html';
+
+  $rootScope.$on('login-logged-in', function () {
+    factory.userLoginTemplate = 'dist/html/components/menu/user.html';
+  });
+
+  $rootScope.$on('login-logged-out', function () {
+    factory.userLoginTemplate = 'dist/html/components/menu/login-or-register.html';
+  });
+
+  factory.userData = mnUserFactory.userData;
+
   // TODO: Check if isCollapsed is the wrong word. Should it be isExpanded?
   // For compressed menu when viewing with a small screen.
-  factory.isCollapsed = true;
+  factory.isExpanded = true;
+
+
 
   return factory;
 }];
